9e9d7d7c9a4ffdd47295623b87e86390809a2a17
Hugo Carrizo Manzoni <hugocmdesign@gmail.com>
2020-01-24 16:43:03 +0100

    Setup the hook for the products, showing up the products on index page, create all the CSS and components

---
+++ hooks/useProductos.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState, useContext } from 'react'
+import { FirebaseContext } from '../firebase'
+
+
+const useProductos = orden => {
+
+    const [productos, setProductos] = useState([]);
+  
+    const { firebase } = useContext(FirebaseContext);
+  
+    useEffect( () => {
+      const obtenerProductos = () => {
+        firebase.db.collection('productos').orderBy(orden, 'desc').onSnapshot(handleSnapshot)
+      }
+      obtenerProductos();
+    }, []);
+  
+    function handleSnapshot(snapshot) {
+      const productos = snapshot.docs.map(doc => {
+          return {
+            id: doc.id,
+            ...doc.data()
+          }
+      });
+      setProductos(productos);
+    }
+  
+    return {productos};
+};
+
+export default useProductos;
